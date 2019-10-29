37cd6c51d34caf3623d539f8d9a4f6f3b3d01bfe
Ivy Ng <INg@ucvts.tec.nj.us>
2019-10-29 10:49:29 -0400

    Your mom

--- src/change.js
+++ src/change.js
@@ -1,14 +1,23 @@
 const readlineSync = require("readline-sync");
-
 const MIN = 0.01;
 const MAX = 0.99;
-
-const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
-
+const quarter = Number(0.25);
+const dime = Number(0.10);
+const nickel = Number(0.05);
+const penny = Number(0.01);
+let amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("Invalid.");
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("Invalid.");
 } else {
-    // write your code for making change here
+    const quarter_number = Math.floor(amount/quarter);
+    amount = ((amount*100)%(quarter*100))/100;
+    const dime_number = Math.floor(amount/dime);
+    amount = ((amount*100)%(dime*100))/100;
+    const nickel_number = Math.floor(amount/nickel);
+    amount = ((amount*100)%(quarter*100))/100;
+    const penny_number = Math.floor(amount/penny);
+    console.log();
+    console.log(quarter_number+" quarters, "+dime_number+" dimes, "+nickel_number+" nickels, "+penny_number+" pennies.")
 }
